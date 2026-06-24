a2133d4878630d7a04ce52ea9c5f4628e711dc04
zaka13 <105675209+zaka13X@users.noreply.github.com>
2026-06-24 13:42:52 -0500

    Update and rename panickkey.js to panick.js

--- scripts/panick.js
+++ scripts/panick.js
@@ -1,5 +1,5 @@
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Escape') {
-        window.top.location.href = 'https://www.powerschool.com/';
+        window.top.location.href = 'https://www.google.com/';
     }
 });
